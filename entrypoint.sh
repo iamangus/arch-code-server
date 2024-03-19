fc29a27d4c0f917215f903b01c8a793090c4e9e0
iamangus <turnbull.angus@gmail.com>
2024-03-19 13:29:30 -0500

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,5 +12,3 @@ sudo chown -R ${USER_NAME}:${USER_NAME} /home/${USER_NAME}
 sudo echo "${USER_NAME} ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/${USER_NAME}
 
 su $USER_NAME --command "code-server --auth none --bind-addr 0.0.0.0:8080 '/home/${USER_NAME}'"
-
-#code-server --auth none --bind-addr 0.0.0.0:8080 "/home/${USER_NAME}"
